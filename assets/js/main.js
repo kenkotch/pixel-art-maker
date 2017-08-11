0f8eb9d5632d27bb5641f883c34d9c794e9a9e51
Ken Kotch <hello@ken.uno>
2017-08-11 15:46:05 -0600

    grid sized right, css hover, js makes grid

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+//grid
+let position = document.querySelector('.grid')
+for (var i = 0; i < 720; i++) {
+  let rowDiv = document.createElement('div')
+  rowDiv.classList.add('box')
+  position.appendChild(rowDiv)
+}
+
+//eventListener
+ 
+
+
+
+
+
+
+})
