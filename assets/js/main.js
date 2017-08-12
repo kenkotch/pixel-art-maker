b27f31fbd080747b2c3c9d1872346c353dd8a4b4
Ken Kotch <hello@ken.uno>
2017-08-12 14:41:31 -0600

    painting with colors works!

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,21 +12,21 @@ function gridMaker(numOfBoxes) {
   //eventListener-box
   var listen = document.querySelector('.grid')
   listen.addEventListener('click', function() {
-    let pix = document.getElementById('event.target.id')
+    // let pix = document.getElementById('event.target.id')
     console.log(event.target.id);
-    event.target.classList.toggle('purple')
+    event.target.classList.toggle(currentColor)
   })
 }
 
   //color swatch
 function colorSwatch() {
+   currentColor = 'red'
   let colorGrid = document.querySelector('.colorContain')
-  const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', '.black', 'white']
+  const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black', 'white']
   for (var i = 0; i < colors.length; i++) {
     let color = document.createElement('div')
     color.classList.add('colorGrid')
     color.style.backgroundColor = colors[i]
-
     let colorID = color.id = (`${colors[i]}`)
     colorGrid.appendChild(color)
   }
@@ -34,9 +34,11 @@ function colorSwatch() {
   var paint = document.querySelector('.colorContain')
   paint.addEventListener('click', function() {
     let col = document.getElementById('event.target.id')
-    console.log(event.target.id);
+    currentColor = event.target.id
+    console.log(currentColor)
+    // col.add(event.target.id)
+    // console.log(col);
   })
-
 }
 
 
