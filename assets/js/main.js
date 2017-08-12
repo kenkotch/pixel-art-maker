b35041489ab924a0289bddd8416644cd3c025651
Ken Kotch <hello@ken.uno>
2017-08-12 16:02:03 -0600

    bug fixes: painted boxws no longer randomly disappear

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,47 +6,38 @@ function gridMaker(numOfBoxes) {
   for (var i = 0; i < numOfBoxes; i++) {
     let pixel = document.createElement('div')
     pixel.classList.add('box')
-    let pixelID = pixel.id = (`pixel${[i]}`)
+    pixel.id = (`pixel${[i]}`)
     position.appendChild(pixel)
   }
   //eventListener-box
-  var listen = document.querySelector('.grid')
+  let listen = document.querySelector('.grid')
   listen.addEventListener('click', function() {
-    // let pix = document.getElementById('event.target.id')
-    console.log(event.target.id);
+    // console.log(event.target.id)
     event.target.classList.toggle(currentColor)
   })
 }
 
   //color swatch
 function colorSwatch() {
-   currentColor = 'red'
+  currentColor = 'red' //  global variable
   let colorGrid = document.querySelector('.colorContain')
-  const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black', 'white']
+  const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'black', 'white']
   for (var i = 0; i < colors.length; i++) {
     let color = document.createElement('div')
     color.classList.add('colorGrid')
     color.style.backgroundColor = colors[i]
-    let colorID = color.id = (`${colors[i]}`)
+    color.id = (colors[i])
     colorGrid.appendChild(color)
   }
   // eventListener-colors
-  var paint = document.querySelector('.colorContain')
+  let paint = document.querySelector('.colorContain')
   paint.addEventListener('click', function() {
-    let col = document.getElementById('event.target.id')
     currentColor = event.target.id
     console.log(currentColor)
-    // col.add(event.target.id)
-    // console.log(col);
   })
 }
 
-
-
-
-
-
+//  call functions
 gridMaker(972)
 colorSwatch()
-
 })
