7c80d4f10a7aa2c38498c984db355a0471bcca80
Ken Kotch <hello@ken.uno>
2017-08-12 13:24:48 -0600

    all boxes and colors have id, color boxes are painted

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,50 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
 
-//grid
-let position = document.querySelector('.grid')
-for (var i = 0; i < 720; i++) {
-  let rowDiv = document.createElement('div')
-  rowDiv.classList.add('box')
-  position.appendChild(rowDiv)
+  //grid
+function gridMaker(numOfBoxes) {
+  let position = document.querySelector('.grid')
+  for (var i = 0; i < numOfBoxes; i++) {
+    let pixel = document.createElement('div')
+    pixel.classList.add('box')
+    let pixelID = pixel.id = (`pixel${[i]}`)
+    position.appendChild(pixel)
+  }
+  //eventListener-box
+  var listen = document.querySelector('.grid')
+  listen.addEventListener('click', function() {
+    let pix = document.getElementById('event.target.id')
+    console.log(event.target.id);
+    event.target.classList.toggle('purple')
+  })
 }
 
-//eventListener
- 
+  //color swatch
+function colorSwatch() {
+  let colorGrid = document.querySelector('.colorContain')
+  const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', '.black', 'white']
+  for (var i = 0; i < colors.length; i++) {
+    let color = document.createElement('div')
+    color.classList.add('colorGrid')
+    color.style.backgroundColor = colors[i]
+
+    let colorID = color.id = (`${colors[i]}`)
+    colorGrid.appendChild(color)
+  }
+  // eventListener-colors
+  var paint = document.querySelector('.colorContain')
+  paint.addEventListener('click', function() {
+    let col = document.getElementById('event.target.id')
+    console.log(event.target.id);
+  })
+
+}
 
 
 
 
 
+
+gridMaker(972)
+colorSwatch()
 
 })
