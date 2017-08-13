8e3a88c3ed94315bfb740609da4d12fc15f404c8
Ken Kotch <hello@ken.uno>
2017-08-13 11:51:18 -0600

    added currrent color box, but only works in certain order

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ function gridMaker(numOfBoxes) {
   }
   //eventListener-box
   let listen = document.querySelector('.grid')
-  listen.addEventListener('click', function() {
+  listen.addEventListener('mousedown', function() {
     // console.log(event.target.id)
     event.target.classList.toggle(currentColor)
   })
@@ -20,8 +20,8 @@ function gridMaker(numOfBoxes) {
   //color swatch
 function colorSwatch() {
   currentColor = 'red' //  global variable
-  let colorGrid = document.querySelector('.colorContain')
-  const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'black', 'white']
+  colorGrid = document.querySelector('.colorContain')
+  const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'black', 'white', 'gold']
   for (var i = 0; i < colors.length; i++) {
     let color = document.createElement('div')
     color.classList.add('colorGrid')
@@ -30,14 +30,55 @@ function colorSwatch() {
     colorGrid.appendChild(color)
   }
   // eventListener-colors
-  let paint = document.querySelector('.colorContain')
+  paint = document.querySelector('.colorContain')
   paint.addEventListener('click', function() {
     currentColor = event.target.id
-    console.log(currentColor)
+
+    //add currentColor to brushColor
+    currentPaint.classList.remove(currentColor)
+    // console.log(currentPaint)
+    currentPaint.classList.add(currentColor)
+    // console.log('currentColor is ' + currentColor)
+    // console.log('currentPaint is ' + currentPaint)
   })
 }
+
+  // brush color indicator on right side
+function brushColor() {
+  currentPaint = document.createElement('div')
+  currentPaint.classList.add('selectedColor')
+  colorGrid.appendChild(currentPaint)
+  console.log(currentPaint);
+}
+
+
+
+  // function colorSwatch() {
+  //   currentColor = 'red' //  global variable
+  //   let colorGrid = document.querySelector('.colorContain')
+  //   const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'black', 'white', 'gold']
+  //   for (var i = 0; i < colors.length; i++) {
+  //     let color = document.createElement('div')
+  //     color.classList.add('colorGrid')
+  //     color.style.backgroundColor = colors[i]
+  //     color.id = (colors[i])
+  //     colorGrid.appendChild(color)
+  //   }
+
+
+  // let brush = document.createElement('div')
+  // // brush.classList.add(.selectedColor)
+  // brush.lastChild(container)
+  // console.log(`brush: ${brush}`)
+  // console.log(`currentColor ${currentColor}`)
+
+  // li.innerHTML = text
+  // ol.appendChild(li)
+
+
 
 //  call functions
 gridMaker(972)
 colorSwatch()
+brushColor()
 })
